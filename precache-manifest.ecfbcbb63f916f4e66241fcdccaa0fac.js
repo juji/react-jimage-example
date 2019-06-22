d687fa3546b59cd276137a889180b8ae6f764606
juji <jujiyangasli@gmail.com>
2019-06-22 22:00:43 +0700

    Updates

---
+++ precache-manifest.ecfbcbb63f916f4e66241fcdccaa0fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9886d9cca3387aca171d6fada6303ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f651571f1f37062cd0",
+    "url": "/static/css/main.ff2bdecf.chunk.css"
+  },
+  {
+    "revision": "23a63c2c7cd60be5663a",
+    "url": "/static/js/2.230e0367.chunk.js"
+  },
+  {
+    "revision": "77f651571f1f37062cd0",
+    "url": "/static/js/main.21e0cd36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
